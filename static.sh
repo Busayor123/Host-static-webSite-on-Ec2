bf30e02c238730d0b63e2f100259649b2b6d0b35
Busayor123 <131611646+Busayor123@users.noreply.github.com>
2023-11-25 13:38:25 -0500

    Update static.sh

--- static.sh
+++ static.sh
@@ -5,7 +5,9 @@
  cd /var/www/html
  wget https://github.com/Busayor123/Host-static-webSite-on-Ec2/archive/refs/heads/main.zip
  unzip main.zip
+ cd Host-static-webSite-on-Ec2-main/
+ unzip mole.zip
  cp -r  mole-main/* /var/www/html/ 
- rm -rf main.zip
+ rm -rf main.zip Host-static-webSite-on-Ec2-main
  systemctl enable httpd
  systemctl start httpd
